f325a2988402b452e0a9799e603b287ba0a26994
kidfox2001 <35247418+kidfox2001@users.noreply.github.com>
2018-06-10 16:53:01 +0700

    6-10-2018

---
+++ Phoneword/Phoneword/TestHorizontalPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Phoneword
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TestHorizontalPage : ContentPage
+	{
+		public TestHorizontalPage ()
+		{
+			InitializeComponent ();
+
+            //Padding = new Thickness(20);
+            var red = new Label
+            {
+                Text = "Stop",
+                BackgroundColor = Color.Red,
+                FontSize = 20,
+                WidthRequest = 100
+            };
+            var yellow = new Label
+            {
+                Text = "Slow down",
+                BackgroundColor = Color.Yellow,
+                FontSize = 20,
+                WidthRequest = 100
+            };
+            var green = new Label
+            {
+                Text = "Go",
+                BackgroundColor = Color.Green,
+                FontSize = 20,
+                WidthRequest = 200
+            };
+
+            Content = new StackLayout
+            {
+                Spacing = 40,
+                VerticalOptions = LayoutOptions.End,
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.Start,
+                Children = { red, yellow, green }
+            };
+
+        }
+	}
+}
---
+++ Phoneword/Phoneword/TestListViewPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Phoneword
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestListViewPage : ContentPage
+    {
+        public TestListViewPage()
+        {
+            InitializeComponent();
+
+            var listView = new ListView
+            {
+                RowHeight = 40
+            };
+
+            listView.ItemsSource = new TodoItem[] {
+                new TodoItem { Name = "Buy pears" },
+                new TodoItem { Name = "Buy oranges", Done=true} ,
+                new TodoItem { Name = "Buy mangos" },
+                new TodoItem { Name = "Buy apples", Done=true },
+                new TodoItem { Name = "Buy bananas", Done=true }
+                };
+
+            listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+            listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+            listView.ItemSelected += async (sender, e) =>
+            {
+                await DisplayAlert("Tapped!", ((TodoItem)e.SelectedItem).Name + " was tapped.", "OK");
+            };
+
+            //listView.ItemSelected += async (sender, e) => {
+            //    var todoItem = (TodoItem)e.SelectedItem;
+            //    var todoPage = new TodoItemPage(todoItem); // so the new page shows correct data
+            //    await Navigation.PushAsync(todoPage);
+            //};
+
+            // ex (แบบธรรมดา)        
+
+            //        listView.ItemsSource = new string[]
+            //        {
+            //"Buy pears", "Buy oranges", "Buy mangos", "Buy apples", "Buy bananas"
+            //        };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = { listView }
+            };
+
+
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+}
